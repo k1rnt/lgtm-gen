888fa49c56cd0698c2df13f4ab83904f3e0f4dae
k1rnt <eru08tera15mora@gmail.com>
2022-09-03 11:18:17 +0900

    ping

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{middleware, web, App, HttpServer, Result, HttpResponse};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Ping {
+    message: String,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .configure(routes)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+fn routes(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("/api/v1")
+            .service(web::resource("/ping").to(ping)),
+    );
+}
+
+async fn ping() -> HttpResponse {
+    HttpResponse::Ok().json(Ping {
+        message: "pong".to_string(),
+    })
 }
